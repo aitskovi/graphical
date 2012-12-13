e7d637c4d5774d4d0499c71e1391e76abcffe5d3
Avi Itskovich <avi.itskovich@gmail.com>
2012-12-13 13:43:12 -0500

    Add matching edges

--- graph.js
+++ graph.js
@@ -1,3 +1,7 @@
+/**
+ * Graph wrapper based off of http://bl.ocks.org/1095795
+ */
+
 function Graph() {
     var width = 960,
         height = 500;
@@ -6,6 +10,7 @@ function Graph() {
 
     this.nodes = [];
     this.links = [];
+    this.matching = [];
 
     var force = d3.layout.force()
         .nodes(this.nodes)
@@ -20,12 +25,25 @@ function Graph() {
         .attr("height", height);
 
     var node = svg.selectAll(".node"),
-        link = svg.selectAll(".link");
+        link = svg.selectAll(".link .non-matching"),
+        match = svg.selectAll(".link .matching");
 
     this.update = function() {
-        link = link.data(force.links(), function(d) {return d.source.id + "-" + d.target.id; });
-        link.enter().insert("line", ".node").attr("class", "link");
+        edges = force.links().filter(function(edge) {
+            return this.matching.indexOf(edge) < 0;
+        }.bind(this));
+
+        matchingEdges = force.links().filter(function(edge) {
+            return this.matching.indexOf(edge) >= 0;
+        }.bind(this));
+
+        link = link.data(edges, function(d) {return d.source.id + "-" + d.target.id; });
+        link.enter().insert("line", ".node").attr("class", "link non-matching");
         link.exit().remove();
+
+        match = match.data(matchingEdges, function(d) {return d.source.id + "-" + d.target.id; });
+        match.enter().insert("line", ".node").attr("class", "link matching");
+        match.exit().remove();
         
         node = node.data(force.nodes(), function(d) { return d.id; });  
         node.enter().append("circle").attr("class", function(d) { return "node " + d.id; }).attr("r", 8);
@@ -39,7 +57,12 @@ function Graph() {
                 .attr("y1", function(d) { return d.source.y; })
                 .attr("x2", function(d) { return d.target.x; })
                 .attr("y2", function(d) { return d.target.y; });
-             
+
+            match.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
             node.attr("cx", function(d) { return d.x; })
                 .attr("cy", function(d) { return d.y; });
     }
@@ -66,9 +89,9 @@ Graph.prototype.removeNode = function(id) {
     this.update();
 }
 
-Graph.prototype.addLink = function(a, b) {
-    a = this.findNode(a);
-    b = this.findNode(b);
+Graph.prototype.addLink = function(link) {
+    a = this.findNode(link[0]);
+    b = this.findNode(link[1]);
     if (!a || !b) return;
 
     this.links.push({source:a, target:b});
@@ -123,28 +146,25 @@ Graph.prototype.indicesOfLinks = function(id) {
 
     return indices;
 }
-    
-//  this.removeNode = function (id) {
-//      var i = 0;
-//      var n = findNode(id);
-//      while (i < links.length) {
-//          if ((links[i]['source'] == n)||(links[i]['target'] == n)) links.splice(i,1);
-//          else i++;
-//      }
-//      nodes.splice(findNodeIndex(id),1);
-//      update();
-//  }
 
-//  this.addLink = function (source, target) {
-//      links.push({"source":findNode(source),"target":findNode(target)});
-//      update();
-//  }
+Graph.prototype.updateMatching = function(links) {
+    this.matching = links
+        .filter(function(link) { return this.indexOfLink(link[0], link[1]) >= 0 }.bind(this))
+        .map(function(link) { return this.links[this.indexOfLink(link[0], link[1])]; }.bind(this));
+    this.update();
+}
+    
 graph = new Graph();
 
-// You can do this from the console as much as you like...
-//graph.addNode("Cause");
-//graph.addNode("Effect");
-//graph.addLink("Cause", "Effect");
-//graph.addNode("A");
-//graph.addNode("B");
-//graph.addLink("A", "B");
+graph.addNode("A");
+graph.addNode("B");
+graph.addNode("C");
+graph.addNode("D");
+graph.addNode("E");
+graph.addNode("F");
+graph.addLink(["A", "B"]);
+graph.addLink(["A", "C"]);
+graph.addLink(["A", "D"]);
+graph.addLink(["D", "E"]);
+graph.addLink(["C", "F"]);
+graph.updateMatching([["A", "B"], ["D", "E"], ["C", "F"]]);
